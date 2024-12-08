43e893b66035858079c494edd0d9e39df2297706
Jurriaan Brandsma <jurriaan.brandsma@gmail.com>
2024-12-08 17:41:02 +0100

    Update installer

--- 3. Dataplane-on-k8s-on-WSL/bin/installdataplaneonddforwsl.sh
+++ 3. Dataplane-on-k8s-on-WSL/bin/installdataplaneonddforwsl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -fsSL https://raw.githubusercontent.com/tibco-bnl/workshop-tibco-platform/dataplane-on-k8s-in-wsl/3.%20Dataplane-on-k8s-on-WSL/bin/replaceguienvvar.sh -o  replaceguienvvar.sh
-chmod +x replaeguienvvar.sh
+chmod +x replaceguienvvar.sh
 curl -fsSL https://raw.githubusercontent.com/TIBCOSoftware/platform-provisioner/master/docs/recipes/tp-base/tp-base-on-prem.yaml -o tp-base-on-prem.yaml
 export PIPELINE_INPUT_RECIPE="tp-base-on-prem.yaml"
 ./replaceguienvvar.sh GUI_TP_INSTALL_PROVISIONER_UI \"false\" tp-base-on-prem.yaml
